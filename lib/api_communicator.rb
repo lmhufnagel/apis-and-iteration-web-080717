a9022b006fd409b1150074e3c25d714f9eb7373c
lmhufnagel <lmhufnagel@gmail.com>
2017-08-08 13:52:08 -0400

    done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,8 +6,20 @@ def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
+
+character_array = character_hash["results"]
+films_hash = character_array[0]["films"]
+
+
+end
+
+# character_array.map do |character_info|
+# if character == character_info["name"]
+#
+#   puts RestClient.get(character_info["films"])
+# end
+
+  # x iterate over the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
@@ -16,17 +28,23 @@ def get_character_movies_from_api(character)
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
-end
+  # film_info = RestClient.get
+
+
+
+
 
 def parse_character_movies(films_hash)
+  films_hash.collect { |film| RestClient.get(film)
+  JSON.parse(request)}
   # some iteration magic and puts out the movies in a nice list
 end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
+  puts films.data.collect { |film| film["title"]}
 end
-
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,10 @@
 def welcome
+  puts "Welcome to Star Wars API!"
   # puts out a welcome message here!
 end
 
 def get_character_from_user
   puts "please enter a character"
+  gets.chomp.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
 end
